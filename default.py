1cdf9dbf4e73f0e0cffd3ad3f2b04f9f5b067ff0
lkf00 <guillermolavadomargallo@gmail.com>
2025-03-09 17:52:27 +0100

    Create default.py

---
+++ default.py
@@ -0,0 +1,17 @@
+import xbmcplugin
+import xbmcgui
+import sys
+
+# Ruta del archivo m3u
+m3u_file = 'https://github.com/tu_usuario/tu_repositorio/raw/main/archivo.m3u'
+
+def listar_videos():
+    with open(m3u_file, 'r') as f:
+        for line in f:
+            if line.startswith('http'):
+                url = line.strip()
+                li = xbmcgui.ListItem(label=url)
+                xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=li)
+
+listar_videos()
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
